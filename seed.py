75e21bbaf22f1149278b8e7ad90fdea4dc52edd8
vagrant <vagrant@vagrant.vm>
2017-11-07 21:28:29 +0000

    load_cities function in seed passes doctest

--- seed.py
+++ seed.py
@@ -1,13 +1,27 @@
 """Utility file to seed cities database"""
 
-from model import City, connect_to_db, db
-from server import app
+# from model import City, connect_to_db, db
+# from server import app
 
 def load_cities():
-    """Load cities from city file"""
+    """Load cities from city file
+
+    >>> load_cities()
+    Cities
+    ['Brazil', '-23.56288', '-46.654659']
+
+    """
 
     print "Cities"
+    # file open 
+    city_file = open("samplecities.txt")
 
-    for line in open("samplecities.txt"):  # might need to specifiy file differently
+    city_dict = {}
+
+    for line in city_file:  # split data and store in dictionary by city
         line = line.rstrip()
-        city, _, lat, lng, population, country_name, _, country_code, _, = line.split(",")
+        country_name, city, latitude, longitude = line.split(",")
+        city_dict[city] = [country_name, latitude, longitude]
+
+    print city_dict['Sao Paulo']
+    city_file.close()
