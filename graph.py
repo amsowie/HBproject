4fc4125d4d3c7e6b0671962eaf7ed4a90e2d2eb2
vagrant <vagrant@vagrant.vm>
2017-11-28 02:59:53 +0000

    basic graph structure file working

---
+++ graph.py
@@ -0,0 +1,41 @@
+class CityNode(object):
+    """Node in a graph representing a city."""
+
+    def __init__(self, city, adjacent=None):
+        """Create a city node with cities adjacent"""
+
+        if adjacent is None:
+            adjacent = {}
+
+        assert isinstance(adjacent, dict), \
+            "adjacent must be a dictionary!"
+
+        self.city = city
+        self.adjacent = adjacent
+
+    def __repr__(self):
+        """Debugging-friendly representation"""
+
+        return "<CityNode: %s>" % self.city
+
+class TripGraph(object):
+    """Graph holding cities and their trip paths."""
+
+    def __init__(self):
+        """Create an empty graph"""
+
+        self.nodes = set()
+
+    def __repr__(self):
+        return "<TripGraph: %s>" % [c.city for c in self.nodes]
+
+    def add_city(self, city):
+        """Add a city to the graph"""
+
+        self.nodes.add(city)
+
+    def set_city_routes(self, city1, city2):
+        """Set two cities as adjacent"""
+
+        city1.adjacent[city1] = {city2: weight}
+        city2.adjacent[city2] = {city1: weight}
